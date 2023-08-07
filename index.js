250e0822eb4319ca2bdcd2e73ff8b20e76728ca8
justShashank57 <justShashank57@gmail.com>
2023-08-08 03:24:49 +0530

    API KEY

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
